64c241e054243c10881e960643e2d6099a076eab
purus6233 <alkomarenko@yandex.ru>
2017-01-16 12:55:28 +0200

    add service implementation

---
+++ src/main/java/com/mental/contactbook/service/impl/ContactServiceImpl.java
@@ -0,0 +1,25 @@
+package com.mental.contactbook.service.impl;
+
+import java.util.Collection;
+
+import com.mental.contactbook.entity.Contact;
+import com.mental.contactbook.service.ContactService;
+
+public class ContactServiceImpl implements ContactService {
+
+	public Contact getContact(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Contact> getAllContactsExceptRegex(String regex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/com/mental/contactbook/service/impl/ContactServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.mental.contactbook.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactServiceImplTest {
+
+	@Test
+	public void test_type() throws Exception {
+		assertNotNull(ContactServiceImpl.class);
+	}
+
+}
